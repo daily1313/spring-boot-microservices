ae48fc24a7bf6b4ae1036454bd289cd55b118651
daily1313 <kimsb7218@naver.com>
2025-03-08 00:39:12 +0900

    chore: set up microservice package for project

--- article-service/src/main/java/board/article/ArticleApplication.java
+++ article-service/src/main/java/board/article/ArticleApplication.java
@@ -1,13 +1,12 @@
-package com.example.springbootmicroservices;
+package board.article;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootMicroservicesApplication {
+public class ArticleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootMicroservicesApplication.class, args);
+        SpringApplication.run(ArticleApplication.class, args);
     }
-
 }
