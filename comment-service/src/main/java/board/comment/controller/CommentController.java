1b42ea9fa168721392b1cab95768c3e612c2d893
daily1313 <kimsb7218@naver.com>
2025-04-24 23:59:46 +0900

    faet: add comment list api for Hierarchical comment

--- comment-service/src/main/java/board/comment/controller/CommentController.java
+++ comment-service/src/main/java/board/comment/controller/CommentController.java
@@ -2,9 +2,12 @@ package board.comment.controller;
 
 import board.comment.service.CommentService;
 import board.comment.service.request.CommentCreateRequest;
+import board.comment.service.response.CommentPageResponse;
 import board.comment.service.response.CommentResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -27,5 +30,24 @@ public class CommentController {
     @DeleteMapping("/v1/comments/{commentId}")
     public void delete(@PathVariable("commentId") Long commentId) {
         commentService.delete(commentId);
+    }
+
+    @GetMapping("/v1/comments")
+    public CommentPageResponse readAll(
+            @PathVariable("articleId") Long articleId,
+            @PathVariable("page") Long page,
+            @PathVariable("pageSize") Long pageSize
+    ) {
+        return commentService.readAll(articleId, page, pageSize);
+    }
+
+    @GetMapping("/v1/comments/infinite-scroll")
+    public List<CommentResponse> readAll(
+            @PathVariable("articleId") Long articleId,
+            @PathVariable("lastParentCommentId") Long lastParentCommentId,
+            @PathVariable("lastCommentId") Long lastCommentId,
+            @PathVariable("pageSize") Long pageSize
+    ) {
+        return commentService.readAll(articleId, lastParentCommentId, lastCommentId, pageSize);
     }
 }
