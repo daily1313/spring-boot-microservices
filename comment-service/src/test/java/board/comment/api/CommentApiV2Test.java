6dad7935073f180b46f72f865484cb833a75f48f
daily1313 <kimsb7218@naver.com>
2025-04-27 23:48:12 +0900

    feat: add comment list api test

--- comment-service/src/test/java/board/comment/api/CommentApiV2Test.java
+++ comment-service/src/test/java/board/comment/api/CommentApiV2Test.java
@@ -17,11 +17,22 @@ public class CommentApiV2Test {
         CommentResponse response2 = create(new CommentCreateRequestV2(2L, "my comment2", response1.getPath(), 1L));
         CommentResponse response3 = create(new CommentCreateRequestV2(2L, "my comment3", response2.getPath(), 1L));
 
-        System.out.println("response1 = " + response1.getPath());
-        System.out.println("\t response2 = " + response2.getPath());
-        System.out.println("\t\t response3 = " + response3.getPath());
-
+        System.out.println("response1.getPath() = " + response1.getPath());
+        System.out.println("response1.getCommentId() = " + response1.getCommentId());
+        System.out.println("\t response2.getPath() = " + response2.getPath());
+        System.out.println("\t response2.getCommentId() = " + response2.getCommentId());
+        System.out.println("\t\t response3.getPath() = " + response3.getPath());
+        System.out.println("\t\t response3.getCommentId() = " + response3.getCommentId());
     }
+
+    /*
+        response1.getPath() = 00004
+        response1.getCommentId() = 174893784257478656
+	    response2.getPath() = 0000400000
+	    response2.getCommentId() = 174893784618188800
+		 response3.getPath() = 000040000000000
+		 response3.getCommentId() = 174893784710463488
+     */
 
     CommentResponse create(CommentCreateRequestV2 request) {
         return restClient.post()
@@ -29,6 +40,23 @@ public class CommentApiV2Test {
                 .body(request)
                 .retrieve()
                 .body(CommentResponse.class);
+    }
+
+    @Test
+    void read() {
+        CommentResponse response = restClient.get()
+                .uri("/v2/comments/{commentId}", 174893784257478656L)
+                .retrieve()
+                .body(CommentResponse.class);
+
+        System.out.println("response = " + response);
+    }
+
+    @Test
+    void delete() {
+        restClient.delete()
+                .uri("/v2/comments/{commentId}", 174893784257478656L)
+                .retrieve();
     }
 
     @Getter
