96304e46b323e70a20509241ab6cfbfff40fda3a
daily1313 <kimsb7218@naver.com>
2025-05-01 18:05:55 +0900

    feat: add simple article-like api & like and unlike api test

---
+++ like-service/src/main/java/board/like/service/ArticleLikeService.java
@@ -0,0 +1,41 @@
+package board.like.service;
+
+
+import board.common.snowflake.Snowflake;
+import board.like.entity.ArticleLike;
+import board.like.repository.ArticleLikeRepository;
+import board.like.service.response.ArticleLikeResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class ArticleLikeService {
+
+    private Snowflake snowflake = new Snowflake();
+    private final ArticleLikeRepository articleLikeRepository;
+
+    public ArticleLikeResponse read(Long articleId, Long userId) {
+        return articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .map(ArticleLikeResponse::from)
+                .orElseThrow();
+    }
+
+    @Transactional
+    public void like(Long articleId, Long userId) {
+        articleLikeRepository.save(
+                ArticleLike.create(
+                        snowflake.nextId(),
+                        articleId,
+                        userId
+                )
+        );
+    }
+
+    @Transactional
+    public void unlike(Long articleId, Long userId) {
+        articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .ifPresent(articleLikeRepository::delete);
+    }
+}
---
+++ like-service/src/main/java/board/like/service/response/ArticleLikeResponse.java
@@ -0,0 +1,29 @@
+package board.like.service.response;
+
+import board.like.entity.ArticleLike;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@ToString
+@Getter
+public class ArticleLikeResponse {
+
+    private Long articleLikeId;
+
+    private Long articleId; // shard key
+
+    private Long userId;
+
+    private LocalDateTime createdAt;
+
+    public static ArticleLikeResponse from(ArticleLike articleLike) {
+        ArticleLikeResponse response = new ArticleLikeResponse();
+        response.articleLikeId = articleLike.getArticleLikeId();
+        response.articleId = articleLike.getArticleId();
+        response.userId = articleLike.getUserId();
+        response.createdAt = articleLike.getCreatedAt();
+        return response;
+    }
+}
