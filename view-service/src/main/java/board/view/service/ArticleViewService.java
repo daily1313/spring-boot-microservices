323757bfc9f15d507aef40b79f2a5c4d678f48f1
daily1313 <kimsb7218@naver.com>
2025-05-02 18:03:16 +0900

    feat: apply article_view_count abusing defense function using distributed lock

--- view-service/src/main/java/board/view/service/ArticleViewService.java
+++ view-service/src/main/java/board/view/service/ArticleViewService.java
@@ -1,18 +1,28 @@
 package board.view.service;
 
 import board.view.repository.ArticleViewCountRepository;
+import board.view.repository.ArticleViewDistributedLockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @RequiredArgsConstructor
 @Service
 public class ArticleViewService {
 
+    private final ArticleViewDistributedLockRepository articleViewDistributedLockRepository;
     private final ArticleViewCountRepository articleViewCountRepository;
     private final ArticleViewCountBackupProcessor articleViewCountBackupProcessor;
+
     private static final int BACK_UP_BATCH_SIZE = 100;
+    private static final Duration ttl = Duration.ofMinutes(10);
 
     public Long increase(Long articleId, Long userId) {
+        if(!articleViewDistributedLockRepository.lock(articleId, userId, ttl)) {
+            return articleViewCountRepository.read(articleId);
+        }
+
         Long count = articleViewCountRepository.increase(articleId);
 
         if(count % BACK_UP_BATCH_SIZE == 0) {
