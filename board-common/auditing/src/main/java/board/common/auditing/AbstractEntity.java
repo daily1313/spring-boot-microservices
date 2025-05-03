9e656807616271c7b3af2bb68b9753271e6771bb
daily1313 <kimsb7218@naver.com>
2025-05-03 14:55:41 +0900

    feat: add jpa auditing common module

---
+++ board-common/auditing/src/main/java/board/common/auditing/AbstractEntity.java
@@ -0,0 +1,30 @@
+package board.common.auditing;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AbstractEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.modifiedAt = createdAt;
+    }
+}
+
